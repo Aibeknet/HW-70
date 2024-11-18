17cf9a777e56e4d0a0f50f304aa54b5b5c31dc23
aibeknet <aaskarov0690.kg@gmail.com>
2024-11-18 13:18:10 +0600

    Add async thunks for contacts: fetch, create, update.

---
+++ src/store/thunks/contactsThunk.ts
@@ -0,0 +1,29 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axiosApi from '../../axiosAPI';
+import { IContact, IContactMutation } from '../../types';
+
+export const fetchAllContacts = createAsyncThunk<IContact[]>('contacts/fetchAll', async () => {
+  const { data } = await axiosApi.get('contacts.json');
+  return Object.keys(data || {}).map((key) => ({
+    ...data[key],
+    id: key
+  }));
+});
+
+export const createContact = createAsyncThunk<IContact, IContactMutation>(
+  'contacts/createContact',
+  async (contactData) => {
+    const { data } = await axiosApi.post('contacts.json', contactData);
+    return { ...contactData, id: data.name };
+  }
+);
+
+export const updateContact = createAsyncThunk<IContact, IContact & { id: string }>(
+  'contacts/updateContact',
+  async (contactData) => {
+    await axiosApi.put(`contacts/${contactData.id}.json`, contactData);
+    return contactData;
+  }
+);
+
+
