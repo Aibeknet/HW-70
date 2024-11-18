b9b4a62a6e8e3f6949c7952a1715e9e124419eb9
aibeknet <aaskarov0690.kg@gmail.com>
2024-11-18 17:32:26 +0600

    Implement routing and contacts loading in `App` component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-const App = () => {
+import React, { useEffect } from 'react';
+import {Routes, Route } from 'react-router-dom';
+import ToolBar from './components/ToolBar/ToolBar';
+import ContactsList from './components/ContactsList/ContactsList';
+import AddContact from './components/AddContact/AddContact';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAllContacts } from './store/thunks/contactsThunk';
+import { selectContacts, selectFetchContactsLoading } from './store/slices/contactsSlice';
+import { AppDispatch } from './app/store.ts';
+
+const App: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const contacts = useSelector(selectContacts);
+  const isLoading = useSelector(selectFetchContactsLoading);
+
+  useEffect(() => {
+    dispatch(fetchAllContacts());
+  }, [dispatch]);
+
   return (
-    <div>
-      Work!
-    </div>
+    <>
+      <ToolBar />
+      <div className="container mt-4">
+        <Routes>
+          <Route
+            path="/"
+            element={<ContactsList contacts={contacts} isLoading={isLoading} />}
+          />
+          <Route path="/add" element={<AddContact />} />
+          <Route path="/edit/:id" element={<AddContact />} />
+        </Routes>
+      </div>
+    </>
   );
 };
 
